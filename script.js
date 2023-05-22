c4ebe9903301743ad7dd52e23227bdb1e6b320bc
Davi Martins <martinslimafkc@gmail.com>
2023-05-22 08:31:43 -0300

    Added a aniamtion to the H1
    Used some Js to animate my h1

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict'
 
+// something new
+let h1Tag = document.querySelector('.main-bio > h1');
+let text = 'avi Martins personal site';
+text.split('').forEach((l, index) =>{
+    setTimeout(() => {
+        h1Tag.innerHTML += l
+    }, 80 * index)
+})
