b1c72244677703fee3ed7cba9c01c22def70af4e
Davi Martins <martinslimafkc@gmail.com>
2023-11-19 16:55:56 -0300

    Changed CV

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 'use strict'
 
-// something new
 let h1Tag = document.querySelector('.main-bio > h1');
 let text = 'avi Martins personal site';
-text.split('').forEach((l, index) =>{
+
+
+//Função para criar animação de digitação
+
+
+//Faz com que  o texto seja colocado dentro de um Array
+let splitedText = text.split('');
+
+// Aplica a função a cada elemento do array
+splitedText.forEach((l, index) => {
+    // Adiciona um atraso para simular a animação de digitação
     setTimeout(() => {
-        h1Tag.innerHTML += l
-    }, 120 * index)
-})
+        // Adiciona cada letra do texto ao conteúdo do elemento h1
+        h1Tag.innerHTML += l;
+    }, 140 * index); // Cada letra é adicionada a cada 140 milissegundos
+});
