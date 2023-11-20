4d67115b0a75ed82857f086b0af12068db1300a4
Davi Martins <martinslimafkc@gmail.com>
2023-11-20 15:41:38 -0300

    Added jQuery to the project

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 'use strict'
 
-let h1Tag = document.querySelector('.main-bio > h1');
+let h1Tag = $('.main-bio > h1');
 let text = 'avi Martins personal site';
 
-
-//Função para criar animação de digitação
-
-
-//Faz com que  o texto seja colocado dentro de um Array
+// Faz com que o texto seja colocado dentro de um Array
 let splitedText = text.split('');
 
-// Aplica a função a cada elemento do array
-splitedText.forEach((l, index) => {
+// Aplica a função a cada elemento do array usando jQuery
+$.each(splitedText, (index, l) => {
     // Adiciona um atraso para simular a animação de digitação
     setTimeout(() => {
-        // Adiciona cada letra do texto ao conteúdo do elemento h1
-        h1Tag.innerHTML += l;
+        // Adiciona cada letra do texto ao conteúdo do elemento h1 usando jQuery
+        h1Tag.append(l);
     }, 140 * index); // Cada letra é adicionada a cada 140 milissegundos
 });
